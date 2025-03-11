552af1fa704fca92c8f5846a843bdaa925d05f85
Laura Carneiro de Azevedo <azevedolaura02@gmail.com>
2025-03-11 15:20:29 -0300

    fix: add BasicSecurityConfig class missing anotations

--- blogpessoal/src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
+++ blogpessoal/src/main/java/com/generation/blogpessoal/security/BasicSecurityConfig.java
@@ -1,14 +1,17 @@
 package com.generation.blogpessoal.security;
 
 import static org.springframework.security.config.Customizer.withDefaults;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 import org.springframework.http.HttpMethod;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -16,6 +19,8 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
+@Configuration
+@EnableWebSecurity
 public class BasicSecurityConfig {
 
 	@Autowired
